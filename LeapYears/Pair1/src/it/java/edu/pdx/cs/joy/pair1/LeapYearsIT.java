f0028d3ec3bc4e33a745b2694976aaef3ba7fb04
David Whitlock <whitlock@cs.pdx.edu>
2024-07-09 19:18:34 -0700

    Leap Years Katas (#1)

---
+++ LeapYears/Pair1/src/it/java/edu/pdx/cs/joy/pair1/LeapYearsIT.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs.joy.pair1;
+
+import edu.pdx.cs.joy.InvokeMainTestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+
+class LeapYearsIT extends InvokeMainTestCase {
+
+  @Test
+  void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(LeapYears.class);
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  }
+
+
+}
---
+++ LeapYears/Pair1/src/test/java/edu/pdx/cs/joy/pair1/LeapYearsTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs.joy.pair1;
+
+import org.junit.jupiter.api.Test;
+
+public class LeapYearsTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new LeapYears();
+  }
+
+}
