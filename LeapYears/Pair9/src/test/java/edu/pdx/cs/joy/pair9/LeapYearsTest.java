be6c2443d7825d2cf8c81cba2efb5439b08de94f
Zhang <zhang13@cs.uoregon.edu>
2024-07-10 18:48:11 -0700

    Created Object test, pair 9.

--- LeapYears/Pair9/src/test/java/edu/pdx/cs/joy/pair9/LeapYearsTest.java
+++ LeapYears/Pair9/src/test/java/edu/pdx/cs/joy/pair9/LeapYearsTest.java
@@ -2,12 +2,17 @@ package edu.pdx.cs.joy.pair9;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class LeapYearsTest
 {
 
   @Test
   void canInstantiateKataClass() {
-    new LeapYears();
+    Object testLeapYears =  new LeapYears();
+    assertThat(testLeapYears, equalTo("A year") );
   }
 
 }
