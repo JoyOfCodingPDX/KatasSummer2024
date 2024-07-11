7c9475f34db049ee7e3dd2f00817c28b9ea3e6dc
isaias valdez <isaiasv@pdx.edu>
2024-07-10 19:52:10 -0700

    Added more LeapYearsIT tests

--- LeapYears/Pair8/src/it/java/edu/pdx/cs/joy/pair8/LeapYearsIT.java
+++ LeapYears/Pair8/src/it/java/edu/pdx/cs/joy/pair8/LeapYearsIT.java
@@ -14,5 +14,23 @@ class LeapYearsIT extends InvokeMainTestCase {
     assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
   }
 
+  @Test
+  void leapYearOutput() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(LeapYears.class, "4");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("is a leap year"));
+  }
+
+  @Test
+  void notLeapYearOutput() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(LeapYears.class, "3");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("is not a leap year"));
+  }
+
+  @Test
+  void nonNumericLeapYearInput() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(LeapYears.class, "L");
+    assertThat(result.getTextWrittenToStandardError(), containsString("Invalid command line arguments"));
+  }
+
 
 }
