29890b6605eab12a36ed9dc7dc5c34b6c4d0161f
Afalsep <junfan@pdx.edu>
2024-07-10 19:04:49 -0700

    test case update

--- LeapYears/Pair4/src/test/java/edu/pdx/cs/joy/pair4/LeapYearsTest.java
+++ LeapYears/Pair4/src/test/java/edu/pdx/cs/joy/pair4/LeapYearsTest.java
@@ -1,5 +1,7 @@
 package edu.pdx.cs.joy.pair4;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class LeapYearsTest
@@ -8,6 +10,15 @@ public class LeapYearsTest
   @Test
   void canInstantiateKataClass() {
     new LeapYears();
+  }
+
+  @Test
+  void testYearMultipleOf400IsLeapYear()
+  {
+    LeapYears year = new LeapYears();
+    boolean result = year.isLeapYear(400);
+
+    assertEquals(result, true);
   }
 
 }
