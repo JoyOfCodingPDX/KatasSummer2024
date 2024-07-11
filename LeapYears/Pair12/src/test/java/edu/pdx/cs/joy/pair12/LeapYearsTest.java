6b287ea608d8d12bc04ab79b95ba19aadbabe676
Olivia Zhang <ozhang@pdx.edu>
2024-07-10 19:10:19 -0700

    first round

--- LeapYears/Pair12/src/test/java/edu/pdx/cs/joy/pair12/LeapYearsTest.java
+++ LeapYears/Pair12/src/test/java/edu/pdx/cs/joy/pair12/LeapYearsTest.java
@@ -2,12 +2,33 @@ package edu.pdx.cs.joy.pair12;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LeapYearsTest
 {
 
   @Test
   void canInstantiateKataClass() {
     new LeapYears();
+  }
+
+  @Test
+  void checkValidYear() {
+    LeapYears leapYear = new LeapYears();
+  }
+
+  @Test
+  void divisibleBy400() {
+    LeapYears leapYear = new LeapYears();
+    assertTrue(LeapYears.leapYear(400));
+  }
+
+  @Test
+  void divisibleBy100ButNot400() {
+    LeapYears leapYear = new LeapYears();
+    assertFalse(LeapYears.leapYear(1700));
   }
 
 }
