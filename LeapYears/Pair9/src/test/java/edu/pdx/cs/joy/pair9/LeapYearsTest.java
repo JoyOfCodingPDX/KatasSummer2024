d6536325d65dad82abfcc970638fc807bccf5618
ProgrammerPouya <45574370+ProgrammerPouya@users.noreply.github.com>
2024-07-10 19:33:54 -0700

    Added a few tests regarding modulos, pair 9

--- LeapYears/Pair9/src/test/java/edu/pdx/cs/joy/pair9/LeapYearsTest.java
+++ LeapYears/Pair9/src/test/java/edu/pdx/cs/joy/pair9/LeapYearsTest.java
@@ -20,9 +20,35 @@ public class LeapYearsTest
 //    LeapYears testLeapYears =  new LeapYears();
 //    assertThat(testLeapYears.getYear().getClass(), equalTo(int.class));
 //  }
+
   @Test
   void yearCanBeDividedBy400() {
     LeapYears testLeapYears =  new LeapYears(400);
     assertThat(testLeapYears.isLeapYear() , equalTo(true));
   }
+
+  @Test
+  void yearCannotBeDividedBy399() {
+    LeapYears testLeapYears =  new LeapYears(399);
+    assertThat(testLeapYears.isLeapYear() , equalTo(false));
+  }
+
+  @Test
+  void leapYearCannotBeDividedBy100() {
+    LeapYears testLeapYears =  new LeapYears(100);
+    assertThat(testLeapYears.isLeapYear() , equalTo(false));
+  }
+
+  @Test
+  void leapYearCanBeDividedBy4() {
+    LeapYears testLeapYears =  new LeapYears(4);
+    assertThat(testLeapYears.isLeapYear() , equalTo(true));
+  }
+
+  @Test
+  void leapYearCannotBeDividedBy2017() {
+    LeapYears testLeapYears =  new LeapYears(2017);
+    assertThat(testLeapYears.isLeapYear() , equalTo(false));
+  }
+
 }
