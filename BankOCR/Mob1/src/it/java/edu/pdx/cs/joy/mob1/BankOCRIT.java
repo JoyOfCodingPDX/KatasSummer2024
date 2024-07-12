9d4aadca5e9273ece93e14896a7d19c861c62fcd
David Whitlock <david.m.whitlock@gmail.com>
2024-07-11 19:36:47 -0700

    BankOCR Mob 1

---
+++ BankOCR/Mob1/src/it/java/edu/pdx/cs/joy/mob1/BankOCRIT.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs.joy.mob1;
+
+import edu.pdx.cs.joy.InvokeMainTestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+
+class BankOCRIT extends InvokeMainTestCase {
+
+  @Test
+  void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(BankOCR.class);
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  }
+
+
+}
