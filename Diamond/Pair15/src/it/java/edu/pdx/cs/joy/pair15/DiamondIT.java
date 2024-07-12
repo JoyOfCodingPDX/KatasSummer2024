02098acc6df2eb27ee9688bb8c688cfa7c68f7ed
David Whitlock <david.m.whitlock@gmail.com>
2024-07-11 19:18:47 -0700

    Diamond Pair 15

---
+++ Diamond/Pair15/src/it/java/edu/pdx/cs/joy/pair15/DiamondIT.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs.joy.pair15;
+
+import edu.pdx.cs.joy.InvokeMainTestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+
+class DiamondIT extends InvokeMainTestCase {
+
+  @Test
+  void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Diamond.class);
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  }
+
+
+}
