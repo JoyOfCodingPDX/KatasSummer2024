1f9a40a4088f613ccf4418a853573b49727fba91
David Whitlock <whitlock@cs.pdx.edu>
2024-07-15 21:33:45 -0700

    Diamond kata projects (#3)

---
+++ Diamond/Pair1/src/test/java/edu/pdx/cs/joy/pair1/DiamondTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs.joy.pair1;
+
+import org.junit.jupiter.api.Test;
+
+public class DiamondTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Diamond();
+  }
+
+}
---
+++ Diamond/Pair9/src/test/java/edu/pdx/cs/joy/pair9/DiamondTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs.joy.pair9;
+
+import org.junit.jupiter.api.Test;
+
+public class DiamondTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Diamond();
+  }
+
+}
