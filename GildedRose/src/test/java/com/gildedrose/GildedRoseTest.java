c3648a3641899ac65a2a1b48ecf47ed4adeb3943
David Whitlock <david.m.whitlock@gmail.com>
2024-07-15 18:58:42 -0700

    Added a unit test to verify the logic of the updateQuality() method.

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,17 +3,34 @@ package com.gildedrose;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Disabled("Reenable when we start doing the kata")
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void itemNameIsRetainedAtEndOfDay() {
+        String itemName = "foo";
+        GildedRose app = addItem(itemName, 0, 0);
+        assertThat(app.items[0].name, equalTo(itemName));
+    }
+
+    @Test
+    void itemQualityDecreasesByOneAtTheEndOfTheDay() {
+        String itemName = "Item";
+        int sellIn = 10;
+        int quality = 20;
+        GildedRose app = addItem(itemName, sellIn, quality);
+        assertThat(app.items[0].sellIn, equalTo(9));
+        assertThat(app.items[0].quality, equalTo(19));
+    }
+
+    private static GildedRose addItem(String itemName, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(itemName, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        return app;
     }
 
 }
