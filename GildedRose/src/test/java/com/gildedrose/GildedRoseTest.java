8b0962defe27b1a1c4bf854c9f7ae5dc2ca800f3
David Whitlock <david.m.whitlock@gmail.com>
2024-07-15 19:03:53 -0700

    Added some unit tests for Aged Brie.

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,5 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,28 +8,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void itemNameIsRetainedAtEndOfDay() {
-        String itemName = "foo";
-        GildedRose app = addItem(itemName, 0, 0);
-        assertThat(app.items[0].name, equalTo(itemName));
-    }
-
-    @Test
-    void itemQualityDecreasesByOneAtTheEndOfTheDay() {
-        String itemName = "Item";
-        int sellIn = 10;
-        int quality = 20;
-        GildedRose app = addItem(itemName, sellIn, quality);
-        assertThat(app.items[0].sellIn, equalTo(9));
-        assertThat(app.items[0].quality, equalTo(19));
-    }
-
-    private static GildedRose addItem(String itemName, int sellIn, int quality) {
+    private static GildedRose addItemAndUpdateQuality(String itemName, int sellIn, int quality) {
         Item[] items = new Item[] { new Item(itemName, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         return app;
     }
 
+    @Test
+    void itemNameIsRetainedAtEndOfDay() {
+        String itemName = "foo";
+        GildedRose app = addItemAndUpdateQuality(itemName, 0, 0);
+        assertThat(app.items[0].name, equalTo(itemName));
+    }
+
+    @Test
+    void itemQualityDecreasesByOneAtTheEndOfTheDay() {
+        GildedRose app = addItemAndUpdateQuality("Item", 10, 20);
+        assertThat(app.items[0].sellIn, equalTo(9));
+        assertThat(app.items[0].quality, equalTo(19));
+    }
+
+    @Test
+    void agedBrieIncreasesInQualityAtTheEndOfTheDay() {
+        GildedRose app = addItemAndUpdateQuality("Aged Brie", 2, 0);
+        assertThat(app.items[0].sellIn, equalTo(1));
+        assertThat(app.items[0].quality, equalTo(1));
+    }
+
+    @Test
+    void qualityOfAgedBrieNeverExceeds50() {
+        GildedRose app = addItemAndUpdateQuality("Aged Brie", 2, 50);
+        assertThat(app.items[0].sellIn, equalTo(1));
+        assertThat(app.items[0].quality, equalTo(50));
+    }
 }
