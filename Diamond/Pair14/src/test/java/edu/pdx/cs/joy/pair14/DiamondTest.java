330306e48f4311930ec168de98e48cf60b4b6f84
Henry Sides <henrycolesides@gmail.com>
2024-07-17 19:50:28 -0700

    First

--- Diamond/Pair14/src/test/java/edu/pdx/cs/joy/pair14/DiamondTest.java
+++ Diamond/Pair14/src/test/java/edu/pdx/cs/joy/pair14/DiamondTest.java
@@ -2,6 +2,8 @@ package edu.pdx.cs.joy.pair14;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DiamondTest
 {
 
@@ -10,4 +12,10 @@ public class DiamondTest
     new Diamond();
   }
 
+  @Test
+  void testFindIndex() {
+    assertEquals(Diamond.get_index("A"), 0);
+    assertEquals(Diamond.get_index("a"), 0);
+    assertEquals(Diamond.get_index("Z"), 25);
+  }
 }
