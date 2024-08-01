ea159b0723d5106a5a39b19378b4e6c5a6a06ea4
Angel Lores <lores@pdx.edu>
2024-07-31 19:22:03 -0700

    added a simple test for empty string

--- BankOCR/Mob4/src/test/java/edu/pdx/cs/joy/mob4/BankOCRTest.java
+++ BankOCR/Mob4/src/test/java/edu/pdx/cs/joy/mob4/BankOCRTest.java
@@ -2,8 +2,16 @@ package edu.pdx.cs.joy.mob4;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class BankOCRTest
 {
+  @Test
+  void emptyStringTest() {
+    //BankOCR bank = new BankOCR();
+    assertThat(BankOCR.doOCR(""), equalTo(""));
+  }
 
   @Test
   void canInstantiateKataClass() {
