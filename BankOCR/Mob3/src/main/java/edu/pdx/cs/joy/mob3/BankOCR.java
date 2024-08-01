98b45efe0946322fd787bc7af089c8afd7de6c0f
Molina Nhoung <molinanhoung@gmail.com>
2024-07-31 19:50:49 -0700

    Molina

--- BankOCR/Mob3/src/main/java/edu/pdx/cs/joy/mob3/BankOCR.java
+++ BankOCR/Mob3/src/main/java/edu/pdx/cs/joy/mob3/BankOCR.java
@@ -2,6 +2,8 @@ package edu.pdx.cs.joy.mob3;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import java.util.HashMap;
+
 /**
  * A class for getting started with a code kata
  *
@@ -16,9 +18,15 @@ public class BankOCR {
       System.err.println("Too few command line arguments");
       return;
     }
+    HashMap<String, Integer> numbers = new HashMap<String, Integer>();
+//    numbers.put("");
+
+    for (int i = 0; i < 9; i++) {
+    }
+
     String line1 = args[0];
     String line2 = args[1];
     String line3 = args[2];
-    System.out.printf("%s\n%s\n%s\n");
+    System.out.printf("%s\n%s\n%s\n", line1, line2, line3);
   }
 }
