219770fb8254bb2a7e996656abb0203cb917939c
Henry Sides <henrycolesides@gmail.com>
2024-07-31 19:40:43 -0700

    Henry

--- BankOCR/Mob3/src/main/java/edu/pdx/cs/joy/mob3/BankOCR.java
+++ BankOCR/Mob3/src/main/java/edu/pdx/cs/joy/mob3/BankOCR.java
@@ -12,6 +12,13 @@ public class BankOCR {
 
   @VisibleForTesting
   public static void main(String[] args) {
-    System.out.print(args[0]);
+    if(args.length < 3) {
+      System.err.println("Too few command line arguments");
+      return;
+    }
+    String line1 = args[0];
+    String line2 = args[1];
+    String line3 = args[2];
+    System.out.printf("%s\n%s\n%s\n");
   }
 }
