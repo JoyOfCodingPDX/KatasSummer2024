f1f79829c57e8df5e576e76a198622d14f2e3f45
Tommy Trakoolthai <77364631+ttrakoolthai@users.noreply.github.com>
2024-07-31 19:30:04 -0700

    Added initial test and parse() method

--- BankOCR/Mob2/src/test/java/edu/pdx/cs/joy/mob2/BankOCRTest.java
+++ BankOCR/Mob2/src/test/java/edu/pdx/cs/joy/mob2/BankOCRTest.java
@@ -2,12 +2,21 @@ package edu.pdx.cs.joy.mob2;
 
 import org.junit.jupiter.api.Test;
 
-public class BankOCRTest
-{
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class BankOCRTest {
 
   @Test
   void canInstantiateKataClass() {
     new BankOCR();
+  }
+
+  @Test
+  void canParseTheNumber1() {
+    BankOCR bankOCR = new BankOCR();
+
+   assertThat(bankOCR.parse("1"), equalTo(1));
   }
 
 }
