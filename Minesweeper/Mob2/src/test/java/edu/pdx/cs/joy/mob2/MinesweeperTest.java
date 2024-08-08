dc7a17bcd6ddf99c12b0f2d9ef82a922f0f9ec76
isaias valdez <isaiasv@pdx.edu>
2024-08-07 18:57:02 -0700

    added diamondString() function

--- Minesweeper/Mob2/src/test/java/edu/pdx/cs/joy/mob2/MinesweeperTest.java
+++ Minesweeper/Mob2/src/test/java/edu/pdx/cs/joy/mob2/MinesweeperTest.java
@@ -2,12 +2,24 @@ package edu.pdx.cs.joy.mob2;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class MinesweeperTest
 {
 
   @Test
   void canInstantiateKataClass() {
     new Minesweeper();
+  }
+
+  @Test
+  void validateInput() {
+    Minesweeper mine = new Minesweeper();
+    mine.parseInput("4", "4");
+    int row = mine.getRow();
+    int col = mine.getCol();
+    assertThat(String.valueOf(col), equals(4));
+
   }
 
 }
