309142fc1c240a36fba17eb6dd621e243db36290
dazl23 <16158572+dazl23@users.noreply.github.com>
2024-08-07 19:45:59 -0700

    Progress on board class implementation

--- Minesweeper/Mob2/src/main/java/edu/pdx/cs/joy/mob2/BoardClass.java
+++ Minesweeper/Mob2/src/main/java/edu/pdx/cs/joy/mob2/BoardClass.java
@@ -1,6 +1,16 @@
 package edu.pdx.cs.joy.mob2;
 
 public class BoardClass {
+    BoardClass(char[][] inputBoard) {
+        this.inputBoard = inputBoard;
+    }
 
+    char[][] resultBoard;
+    char[][] inputBoard;
 
+    char[][] incrementBoard() {
+        for (int row = 0; i < inputBoard.length; ++i) {
+            for (int column = 0; i < inputBoard[row].length);
+        }
+    }
 }
---
+++ Minesweeper/Mob2/src/test/java/edu/pdx/cs/joy/mob2/BoardClassTest.java
@@ -0,0 +1,24 @@
+package edu.pdx.cs.joy.mob2;
+
+import org.junit.jupiter.api.Test;
+
+public class BoardClassTest {
+
+    @Test
+    void testBoard() {
+        char [][] input = {
+                {'*', '0', '0', '0'},
+                {'0', '0', '0', '0'},
+                {'0', '*', '0', '0'},
+                {'0', '0', '0', '0'},
+        };
+        char [][] expected = {
+                {'*', '1', '0', '0'},
+                {'2', '2', '1', '0'},
+                {'1', '*', '1', '0'},
+                {'1', '1', '1', '0'},
+        };
+        BoardClass bc = new BoardClass(input);
+        assertThat(bc.incrementBoard(), equals(expected));
+    }
+}
