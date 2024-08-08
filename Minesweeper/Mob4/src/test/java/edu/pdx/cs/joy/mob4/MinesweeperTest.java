ad88003bd0ce18ba1785a351f979455125ea6807
abhoward9 <32381914+abhoward9@users.noreply.github.com>
2024-08-07 18:55:45 -0700

    a

--- Minesweeper/Mob4/src/test/java/edu/pdx/cs/joy/mob4/MinesweeperTest.java
+++ Minesweeper/Mob4/src/test/java/edu/pdx/cs/joy/mob4/MinesweeperTest.java
@@ -2,12 +2,21 @@ package edu.pdx.cs.joy.mob4;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class MinesweeperTest
 {
 
   @Test
   void canInstantiateKataClass() {
+
     new Minesweeper();
   }
+
+  @Test
+  void canGetBoardSize() {
+    assertThat(Minesweeper.getBoardSize(), new int[]{2, 3});
+  }
+
 
 }
